683fcbcff9d8b82030333f4fadd4510adc68e0d6
Maxi Rosson <maxirosson@gmail.com>
2016-11-18 23:59:47 -0300

    Changed Android minimumSdkVersion to 16  Close #18

--- src/main/groovy/com/jdroid/gradle/android/AndroidGradlePlugin.groovy
+++ src/main/groovy/com/jdroid/gradle/android/AndroidGradlePlugin.groovy
@@ -26,7 +26,7 @@ public abstract class AndroidGradlePlugin extends JavaBaseGradlePlugin {
 			}
 		}
 
-		minimumSdkVersion = jdroid.getIntegerProp('MIN_SDK_VERSION', 15)
+		minimumSdkVersion = jdroid.getIntegerProp('MIN_SDK_VERSION', 16)
 
 		android.compileSdkVersion 25
 		// http://developer.android.com/tools/revisions/build-tools.html
