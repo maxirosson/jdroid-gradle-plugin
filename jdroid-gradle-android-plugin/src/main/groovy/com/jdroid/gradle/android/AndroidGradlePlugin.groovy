d7b2fd266596282a9ce448ac58dd55c05858e0a8
Maxi Rosson <maxirosson@gmail.com>
2017-05-03 20:28:41 -0300

    Upgrade to Android Gradle Plugin v2.3.1. Close #29 Upgrade to build tools v25.0.3. Close #30

--- jdroid-gradle-android-plugin/src/main/groovy/com/jdroid/gradle/android/AndroidGradlePlugin.groovy
+++ jdroid-gradle-android-plugin/src/main/groovy/com/jdroid/gradle/android/AndroidGradlePlugin.groovy
@@ -30,7 +30,7 @@ public abstract class AndroidGradlePlugin extends JavaBaseGradlePlugin {
 
 		android.compileSdkVersion 25
 		// http://developer.android.com/tools/revisions/build-tools.html
-		android.buildToolsVersion "25.0.2"
+		android.buildToolsVersion "25.0.3"
 
 
 		android.defaultConfig {
