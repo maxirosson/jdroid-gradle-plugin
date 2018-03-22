30c93cba01b8a9ec8f0e9d0a692e19e1e4e979fd
Maxi Rosson <maxirosson@gmail.com>
2018-03-22 19:42:27 -0300

    Changed 'StringFormatMatches' and 'StringFormatCount' lint rules severity to error

--- jdroid-gradle-android-plugin/src/main/groovy/com/jdroid/gradle/android/AndroidGradlePlugin.groovy
+++ jdroid-gradle-android-plugin/src/main/groovy/com/jdroid/gradle/android/AndroidGradlePlugin.groovy
@@ -85,6 +85,7 @@ public abstract class AndroidGradlePlugin extends JavaBaseGradlePlugin {
 			abortOnError propertyResolver.getBooleanProp('ABORT_ON_LINT_ERROR', true)
 			enable 'ConvertToWebp'
 			disable 'ContentDescription', 'RtlEnabled', 'RtlHardcoded', 'RtlSymmetry', 'UseCompoundDrawables', 'UnknownIdInLayout', 'RequiredSize'
+			error 'StringFormatMatches', 'StringFormatCount'
 		}
 
 		android.packagingOptions {
