4a15ac92cbfa0e798cd8d686885bf369b29d6d29
Maxi <maxirosson@gmail.com>
2018-11-02 19:27:53 -0300

    Upgraded to google-services plugin v4.2.0

--- jdroid-gradle-android-plugin/src/main/groovy/com/jdroid/gradle/android/AndroidGradlePlugin.groovy
+++ jdroid-gradle-android-plugin/src/main/groovy/com/jdroid/gradle/android/AndroidGradlePlugin.groovy
@@ -15,7 +15,7 @@ public abstract class AndroidGradlePlugin extends JavaBaseGradlePlugin {
 	private static final String ANDROID_BUILD_TOOLS_VERSION = '28.0.3'
 
 	// https://github.com/maxirosson/jdroid-android-lint/releases
-	private static final String JDROID_ANDROID_LINT_RULES_VERSION = "1.3.0"
+	private static final String JDROID_ANDROID_LINT_RULES_VERSION = "1.3.1"
 
 	protected BaseExtension android
 
