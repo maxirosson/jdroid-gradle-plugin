a30275183d0a0973862708debabc1c0f041c7177
Maxi Rosson <maxirosson@gmail.com>
2019-05-26 17:16:49 -0300

    Migrated BuildConfig generation to kotlin

--- jdroid-gradle-java-webapp-plugin/src/main/groovy/com/jdroid/gradle/java/JavaWebAppGradleExtension.groovy
+++ jdroid-gradle-java-webapp-plugin/src/main/groovy/com/jdroid/gradle/java/JavaWebAppGradleExtension.groovy
@@ -13,7 +13,7 @@ public class JavaWebAppGradleExtension extends JavaGradleExtension {
 	}
 
 	public void setBuildConfigString(def out, String key, String defaultValue) {
-		String constant = "		public static final String " + key + " = ";
+		String constant = "	const val " + key + " = ";
 
 		String value = propertyResolver.getStringProp(key, defaultValue)
 		if (value == null) {
@@ -21,8 +21,6 @@ public class JavaWebAppGradleExtension extends JavaGradleExtension {
 		} else {
 			constant += '\"' + value + '\"'
 		}
-
-		constant += ";"
 		out.writeLine(constant)
 	}
 
@@ -31,7 +29,7 @@ public class JavaWebAppGradleExtension extends JavaGradleExtension {
 	}
 
 	public void setBuildConfigBoolean(def out, String key, Boolean defaultValue) {
-		String constant = "		public static final Boolean " + key + " = ";
+		String constant = "	const val " + key + ": Boolean? = ";
 
 		Boolean value = propertyResolver.getBooleanProp(key, defaultValue)
 		if (value == null) {
@@ -39,8 +37,6 @@ public class JavaWebAppGradleExtension extends JavaGradleExtension {
 		} else {
 			constant += value
 		}
-
-		constant += ";"
 		out.writeLine(constant)
 	}
 
@@ -49,7 +45,7 @@ public class JavaWebAppGradleExtension extends JavaGradleExtension {
 	}
 
 	public void setBuildConfigInteger(def out, String key, Integer defaultValue) {
-		String constant = "		public static final Integer " + key + " = ";
+		String constant = "	const val " + key + ": Int? = ";
 
 		Integer value = propertyResolver.getIntegerProp(key, defaultValue)
 		if (value == null) {
@@ -57,8 +53,6 @@ public class JavaWebAppGradleExtension extends JavaGradleExtension {
 		} else {
 			constant += value
 		}
-
-		constant += ";"
 		out.writeLine(constant)
 	}
 
