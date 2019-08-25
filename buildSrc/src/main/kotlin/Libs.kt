18e0ac30d2d3d1f665252f53a1192e38a731020b
Maxi Rosson <maxirosson@gmail.com>
2019-08-24 23:09:12 -0300

    Migrated to buildSrc

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,44 @@
+object Libs {
+
+    // https://github.com/maxirosson/jdroid-java-github/releases
+    const val jdroid_java_github = "com.jdroidtools:jdroid-java-github:1.0.0"
+
+    // https://github.com/maxirosson/jdroid-java/blob/master/CHANGELOG.md
+    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:2.1.0-SNAPSHOT"
+
+    // https://github.com/maxirosson/releases-hub-gradle-plugin
+    const val releases_hub_gradle_plugin = "com.jdroidtools:releases-hub-gradle-plugin:0.1.0-SNAPSHOT"
+
+    // https://plugins.gradle.org/plugin/com.gradle.plugin-publish
+    const val gradle_plugin_publish_plugin = "com.gradle.publish:plugin-publish-plugin:0.9.8"
+
+    // https://developer.android.com/tools/revisions/gradle-plugin.html
+    // https://dl.google.com/dl/android/maven2/index.html
+    const val android_gradle_plugin = "com.android.tools.build:gradle:3.5.0"
+
+    // https://firebase.google.com/docs/perf-mon/get-started-android#add-performance-monitoringto-your-app
+    // https://jcenter.bintray.com/com/google/firebase/perf-plugin/
+    const val firebase_performance_plugin = "com.google.firebase:perf-plugin:1.2.1"
+
+    // https://developers.google.com/android/guides/google-services-plugin
+    // https://jcenter.bintray.com/com/google/gms/google-services/
+    const val google_services_plugin = "com.google.gms:google-services:4.2.0"
+
+    // https://docs.fabric.io/android/changelog.html#fabric-gradle-plugin
+    // https://maven.fabric.io/public/io/fabric/tools/gradle/maven-metadata.xml
+    const val fabric_plugin = "io.fabric.tools:gradle:1.29.0"
+
+    // https://github.com/konifar/gradle-unused-resources-remover-plugin/releases
+    const val unused_resources_remover_plugin = "gradle.plugin.com.github.konifar.gradle:plugin:0.3.3"
+
+    // https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md
+    const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40"
+
+    // https://github.com/Kotlin/dokka/releases
+    const val dokka_plugin = "org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.18"
+
+    // https://github.com/gretty-gradle-plugin/gretty/blob/master/changes.md
+    const val gretty = "org.gretty:gretty:2.3.1"
+
+    const val junit = "junit:junit:4.12"
+}
