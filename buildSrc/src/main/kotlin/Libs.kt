b61055cee46028104acc5505d46e1d167d4a0f4f
jdroid-ci <jdroidtools@gmail.com>
2019-09-06 23:45:42 -0300

    Upgraded dependencies

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -18,21 +18,21 @@ object Libs {
 
     // https://firebase.google.com/docs/perf-mon/get-started-android#add-performance-monitoringto-your-app
     // https://jcenter.bintray.com/com/google/firebase/perf-plugin/
-    const val firebase_performance_plugin = "com.google.firebase:perf-plugin:1.2.1"
+    const val firebase_performance_plugin = "com.google.firebase:perf-plugin:1.3.1"
 
     // https://developers.google.com/android/guides/google-services-plugin
     // https://jcenter.bintray.com/com/google/gms/google-services/
-    const val google_services_plugin = "com.google.gms:google-services:4.2.0"
+    const val google_services_plugin = "com.google.gms:google-services:4.3.2"
 
     // https://docs.fabric.io/android/changelog.html#fabric-gradle-plugin
     // https://maven.fabric.io/public/io/fabric/tools/gradle/maven-metadata.xml
-    const val fabric_plugin = "io.fabric.tools:gradle:1.29.0"
+    const val fabric_plugin = "io.fabric.tools:gradle:1.31.0"
 
     // https://github.com/konifar/gradle-unused-resources-remover-plugin/releases
     const val unused_resources_remover_plugin = "gradle.plugin.com.github.konifar.gradle:plugin:0.3.3"
 
     // https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md
-    const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40"
+    const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50"
 
     // https://github.com/Kotlin/dokka/releases
     const val dokka_plugin = "org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.18"
