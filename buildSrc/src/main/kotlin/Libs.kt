651168e5ffe6e9c8f067b1b5bab2d8f04b51f4fd
jdroid-ci <jdroidtools@gmail.com>
2019-10-20 00:01:41 +0000

    Upgraded com.jdroidtools:jdroid-java-github from 1.0.0 to 1.1.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,7 +1,7 @@
 object Libs {
 
     // https://github.com/maxirosson/jdroid-java-github/releases
-    const val jdroid_java_github = "com.jdroidtools:jdroid-java-github:1.0.0"
+    const val jdroid_java_github = "com.jdroidtools:jdroid-java-github:1.1.0"
 
     // https://github.com/maxirosson/jdroid-java/blob/master/CHANGELOG.md
     const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0"
