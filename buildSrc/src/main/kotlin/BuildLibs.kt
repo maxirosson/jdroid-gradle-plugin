5c92c15b2e8a5eecc5e609356696f446b5714896
Maxi <maxirosson@gmail.com>
2019-11-10 13:43:55 -0300

    Libs and BuildLibs on uppercase

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,10 +1,10 @@
 object BuildLibs {
     // https://github.com/maxirosson/jdroid-gradle-plugin/blob/master/CHANGELOG.md
-    const val jdroid_gradle_root_plugin = "com.jdroidtools:jdroid-gradle-root-plugin:2.3.0"
+    const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.3.0"
 
     // https://github.com/maxirosson/jdroid-gradle-plugin/blob/master/CHANGELOG.md
-    const val jdroid_gradle_project_plugin = "com.jdroidtools:jdroid-gradle-project-plugin:2.3.0"
+    const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:2.3.0"
 
     // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
-    const val jdroid_component_builder = "com.jdroidtools:jdroid-component-builder:2.2.0"
+    const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.2.0"
 }
--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,44 +1,44 @@
 object Libs {
 
     // https://github.com/maxirosson/jdroid-java-github/releases
-    const val jdroid_java_github = "com.jdroidtools:jdroid-java-github:1.1.0"
+    const val JDROID_JAVA_GITHUB = "com.jdroidtools:jdroid-java-github:1.1.0"
 
     // https://github.com/maxirosson/jdroid-java/blob/master/CHANGELOG.md
-    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0"
+    const val JDROID_JAVA_CORE = "com.jdroidtools:jdroid-java-core:3.0.0"
 
     // https://github.com/maxirosson/releases-hub-gradle-plugin
-    const val releases_hub_gradle_plugin = "com.releaseshub:releases-hub-gradle-plugin:1.2.0"
+    const val RELEASES_HUB_GRADLE_PLUGIN = "com.releaseshub:releases-hub-gradle-plugin:1.2.0"
 
     // https://plugins.gradle.org/plugin/com.gradle.plugin-publish
-    const val gradle_plugin_publish_plugin = "com.gradle.publish:plugin-publish-plugin:0.10.1"
+    const val GRADLE_PLUGIN_PUBLISH_PLUGIN = "com.gradle.publish:plugin-publish-plugin:0.10.1"
 
     // https://developer.android.com/tools/revisions/gradle-plugin.html
     // https://dl.google.com/dl/android/maven2/index.html
-    const val android_gradle_plugin = "com.android.tools.build:gradle:3.5.2"
+    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:3.5.2"
 
     // https://firebase.google.com/docs/perf-mon/get-started-android#add-performance-monitoringto-your-app
     // https://jcenter.bintray.com/com/google/firebase/perf-plugin/
-    const val firebase_performance_plugin = "com.google.firebase:perf-plugin:1.3.1"
+    const val FIREBASE_PERFORMANCE_PLUGIN = "com.google.firebase:perf-plugin:1.3.1"
 
     // https://developers.google.com/android/guides/google-services-plugin
     // https://jcenter.bintray.com/com/google/gms/google-services/
-    const val google_services_plugin = "com.google.gms:google-services:4.3.2"
+    const val GOOGLE_SERVICES_PLUGIN = "com.google.gms:google-services:4.3.2"
 
     // https://docs.fabric.io/android/changelog.html#fabric-gradle-plugin
     // https://maven.fabric.io/public/io/fabric/tools/gradle/maven-metadata.xml
-    const val fabric_plugin = "io.fabric.tools:gradle:1.31.2"
+    const val FABRIC_PLUGIN = "io.fabric.tools:gradle:1.31.2"
 
     // https://github.com/konifar/gradle-unused-resources-remover-plugin/releases
-    const val unused_resources_remover_plugin = "gradle.plugin.com.github.konifar.gradle:plugin:0.3.3"
+    const val UNUSED_RESOURCES_REMOVER_PLUGIN = "gradle.plugin.com.github.konifar.gradle:plugin:0.3.3"
 
     // https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md
-    const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50"
+    const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50"
 
     // https://github.com/Kotlin/dokka/releases
-    const val dokka_plugin = "org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.18"
+    const val DOKKA_PLUGIN = "org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.18"
 
     // https://github.com/gretty-gradle-plugin/gretty/blob/master/changes.md
-    const val gretty = "org.gretty:gretty:2.3.1"
+    const val GRETTY = "org.gretty:gretty:2.3.1"
 
-    const val junit = "junit:junit:4.12"
+    const val JUNIT = "junit:junit:4.12"
 }
