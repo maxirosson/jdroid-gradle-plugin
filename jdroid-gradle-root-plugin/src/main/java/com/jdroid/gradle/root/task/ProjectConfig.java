9551bb2790ea968d084b1c928ad19b9d2677899d
Maxi <maxirosson@gmail.com>
2020-01-05 10:58:35 -0300

    Added ktlint pre commit hook

--- jdroid-gradle-root-plugin/src/main/java/com/jdroid/gradle/root/task/ProjectConfig.java
+++ jdroid-gradle-root-plugin/src/main/java/com/jdroid/gradle/root/task/ProjectConfig.java
@@ -9,6 +9,7 @@ public enum ProjectConfig {
 	GIT_IGNORE("/gitignore", ".gitignore", false),
 	CI_IGNORE("/.ciignore", false),
 	EDITOR_CONFIG("/.editorconfig", true),
+	GIT_HOOKS_PRE_COMMIT("/scripts/git/pre-commit", false),
 	GIT_HOOKS_COMMIT_MSG("/scripts/git/commit-msg", false),
 	INIT_GIT_HOOKS("/scripts/git/init_git_hooks.sh", false),
 	GRADLE_WRAPPER_PROPERTIES("/gradle/wrapper/gradle-wrapper.properties", true);
