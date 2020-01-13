1ff0e7ca0ab2233fb015d43f0ebd8a8174b0eb7e
Maxi <maxirosson@gmail.com>
2020-01-13 20:04:44 -0300

    Disabled war publications

--- jdroid-gradle-java-webapp-plugin/src/main/groovy/com/jdroid/gradle/java/JavaWebAppGradlePlugin.groovy
+++ jdroid-gradle-java-webapp-plugin/src/main/groovy/com/jdroid/gradle/java/JavaWebAppGradlePlugin.groovy
@@ -1,7 +1,6 @@
 package com.jdroid.gradle.java
 
 import org.gradle.api.Project
-import org.gradle.api.publish.maven.MavenPublication
 
 public class JavaWebAppGradlePlugin extends JavaGradlePlugin {
 
@@ -17,34 +16,35 @@ public class JavaWebAppGradlePlugin extends JavaGradlePlugin {
 
 		project.sourceSets.main.java.srcDirs += "build/generated"
 
-		if (isPublicationConfigurationEnabled) {
-			def javaWarPublicationsClosure = {
-				javaWar(MavenPublication) {
-					from project.components.web
-					if (isSourcesPublicationEnabled) {
-						artifact project.sourcesJar
-					}
-					if (isJavaDocPublicationEnabled) {
-						artifact project.dokkaJar
-					}
-					pom(createMavenPom())
-
-				}
-			}
-			javaWarPublicationsClosure.setDelegate(project)
-
-			project.publishing {
-				publications(javaWarPublicationsClosure)
-			}
-		}
-
-		if (isSigningPublicationEnabled) {
-			applyPlugin('signing')
-			project.signing {
-				required { true }
-				sign project.publishing.publications.javaWar
-			}
-		}
+		// TODO See if makes sense to allow war publications
+//		if (isPublicationConfigurationEnabled) {
+//			def javaWarPublicationsClosure = {
+//				javaWar(MavenPublication) {
+//					from project.components.web
+//					if (isSourcesPublicationEnabled) {
+//						artifact project.sourcesJar
+//					}
+//					if (isJavaDocPublicationEnabled) {
+//						artifact project.dokkaJar
+//					}
+//					pom(createMavenPom())
+//
+//				}
+//			}
+//			javaWarPublicationsClosure.setDelegate(project)
+//
+//			project.publishing {
+//				publications(javaWarPublicationsClosure)
+//			}
+//		}
+//
+//		if (isSigningPublicationEnabled) {
+//			applyPlugin('signing')
+//			project.signing {
+//				required { true }
+//				sign project.publishing.publications.javaWar
+//			}
+//		}
 	}
 
 	@Override
