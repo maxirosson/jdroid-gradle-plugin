bdd321668c9bb23783354f403b8b927c087d79d7
Maxi <maxirosson@gmail.com>
2020-01-16 23:04:29 -0300

    All the project configs are enabled by default

--- jdroid-gradle-root-plugin/src/main/groovy/com/jdroid/gradle/root/task/ProjectConfig.java
+++ jdroid-gradle-root-plugin/src/main/groovy/com/jdroid/gradle/root/task/ProjectConfig.java
@@ -1,33 +1,33 @@
 package com.jdroid.gradle.root.task;
 
+import org.gradle.api.Project;
+
 public enum ProjectConfig {
 
-	CODE_STYLES_CONFIG("/.idea/codeStyles/codeStyleConfig.xml", true),
-	CODE_STYLES_PROJECT("/.idea/codeStyles/Project.xml", true),
-	INSPECTION_PROFILE("/.idea/inspectionProfiles/profile.xml", true),
-	INSPECTION_PROFILES_PROFILES_SETTINGS("/.idea/inspectionProfiles/profiles_settings.xml", true),
-	GIT_IGNORE("/gitignore", ".gitignore", false),
-	CI_IGNORE("/.ciignore", false),
-	EDITOR_CONFIG("/.editorconfig", true),
-	GIT_HOOKS_PRE_COMMIT("/scripts/git/pre-commit", false),
-	GIT_HOOKS_COMMIT_MSG("/scripts/git/commit-msg", false),
-	INIT_GIT_HOOKS("/scripts/git/init_git_hooks.sh", false),
-	GRADLEW("/gradlew", true),
-	GRADLE_WRAPPER("/gradle/wrapper/gradle-wrapper.jar", true),
-	GRADLE_WRAPPER_PROPERTIES("/gradle/wrapper/gradle-wrapper.properties", true);
+	CODE_STYLES_CONFIG("/.idea/codeStyles/codeStyleConfig.xml"),
+	CODE_STYLES_PROJECT("/.idea/codeStyles/Project.xml"),
+	INSPECTION_PROFILE("/.idea/inspectionProfiles/profile.xml"),
+	INSPECTION_PROFILES_PROFILES_SETTINGS("/.idea/inspectionProfiles/profiles_settings.xml"),
+	GIT_IGNORE("/gitignore", ".gitignore"),
+	CI_IGNORE("/.ciignore"),
+	EDITOR_CONFIG("/.editorconfig"),
+	GIT_HOOKS_PRE_COMMIT("/scripts/git/pre-commit"),
+	GIT_HOOKS_COMMIT_MSG("/scripts/git/commit-msg"),
+	INIT_GIT_HOOKS("/scripts/git/init_git_hooks.sh"),
+	GRADLEW("/gradlew"),
+	GRADLE_WRAPPER("/gradle/wrapper/gradle-wrapper.jar"),
+	GRADLE_WRAPPER_PROPERTIES("/gradle/wrapper/gradle-wrapper.properties");
 
 	private String source;
 	private String target;
-	private boolean strict;
 
-	ProjectConfig(String source, boolean strict) {
-		this(source, source, strict);
+	ProjectConfig(String source) {
+		this(source, source);
 	}
 
-	ProjectConfig(String source, String target, boolean strict) {
+	ProjectConfig(String source, String target) {
 		this.source = source;
 		this.target = target;
-		this.strict = strict;
 	}
 
 	public String getSource() {
@@ -38,7 +38,8 @@ public enum ProjectConfig {
 		return target;
 	}
 
-	public boolean isStrict() {
-		return strict;
+	public Boolean isEnabled(Project project) {
+		// TODO Add support to disable a project config sync through a property
+		return true;
 	}
 }
