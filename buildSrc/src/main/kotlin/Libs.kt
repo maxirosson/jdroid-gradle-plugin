66f43868d5ecafba7050679066cfa7c21ea15c8b
Jdroid CI <jdroidtools@gmail.com>
2020-09-19 17:32:44 -0300

    Upgraded com.android.tools.build:gradle from 4.0.0 to 4.0.1 (#125)

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -14,7 +14,7 @@ object Libs {
 
     // https://developer.android.com/tools/revisions/gradle-plugin.html
     // https://dl.google.com/dl/android/maven2/index.html
-    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:4.0.0"
+    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:4.0.1"
 
     // https://firebase.google.com/docs/perf-mon/get-started-android#add-performance-monitoringto-your-app
     // https://jcenter.bintray.com/com/google/firebase/perf-plugin/
