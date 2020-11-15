f2f4422be2a0e6d98ebee28d3389db303739a861
Maxi Rosson <maxirosson@gmail.com>
2020-11-15 13:56:41 -0300

    Add java version validation

--- jdroid-gradle-root-plugin/src/main/groovy/com/jdroid/gradle/root/task/ProjectConfigValidationTask.java
+++ jdroid-gradle-root-plugin/src/main/groovy/com/jdroid/gradle/root/task/ProjectConfigValidationTask.java
@@ -3,6 +3,9 @@ package com.jdroid.gradle.root.task;
 import com.jdroid.gradle.commons.tasks.AbstractTask;
 import com.jdroid.java.utils.StreamUtils;
 
+import org.gradle.api.GradleException;
+import org.gradle.internal.jvm.Jvm;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -10,12 +13,20 @@ import java.io.FileNotFoundException;
 
 public class ProjectConfigValidationTask extends AbstractTask {
 
+	private static final String EXPECTED_JAVA_VERSION = "11";
+
 	public ProjectConfigValidationTask() {
 		setDescription("Validates if the project configuration is up to date");
 	}
 
 	@Override
 	protected void onExecute() {
+
+		String javaVersion = Jvm.current().getJavaVersion().toString();
+		if (!javaVersion.equals(propertyResolver.getStringProp("EXPECTED_JAVA_VERSION", EXPECTED_JAVA_VERSION))) {
+			throw new GradleException("Wrong java version. Actual version: " + javaVersion + " | Expected version: " + EXPECTED_JAVA_VERSION);
+		}
+
 		File rootDir = getProject().getRootDir();
 
 		for (ProjectConfig projectConfig : ProjectConfig.values()) {
