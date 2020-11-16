17be3d5af489f5d6217cba752b6591261f050f6c
Maxi Rosson <maxirosson@gmail.com>
2020-11-16 13:31:22 -0300

    Upgrade to Gradle 6.7

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.gradle.enterprise").version("3.5")
+}
+
+include(":jdroid-gradle-commons-plugin")
+include(":jdroid-gradle-java-plugin")
+include(":jdroid-gradle-java-webapp-plugin")
+include(":jdroid-gradle-android-plugin")
+include(":jdroid-gradle-project-plugin")
+include(":jdroid-gradle-root-plugin")
+
