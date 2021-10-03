9b47392537ddf142b4da4c4f4d80dd4fec41522a
Maxi Rosson <maxirosson@gmail.com>
2021-10-02 23:30:11 -0300

    Commented broken logic

--- jdroid-gradle-project-plugin/src/main/groovy/com/jdroid/gradle/GradleProjectPlugin.groovy
+++ jdroid-gradle-project-plugin/src/main/groovy/com/jdroid/gradle/GradleProjectPlugin.groovy
@@ -12,40 +12,40 @@ public class GradleProjectPlugin extends JavaGradlePlugin {
 	public void apply(Project project) {
 		super.apply(project)
 
-		project.sourceSets {
-			integrationTest {
-				java.srcDir project.file('src/integrationTest/java')
-				resources.srcDir project.file('src/integrationTest/resources')
-				compileClasspath += project.sourceSets.main.output + project.configurations.testRuntime
-				runtimeClasspath += output + compileClasspath
-			}
-			functionalTest {
-				java.srcDir project.file('src/functionalTest/java')
-				resources.srcDir project.file('src/functionalTest/resources')
-				compileClasspath += project.sourceSets.main.output + project.configurations.testRuntime
-				runtimeClasspath += output + compileClasspath
-			}
-		}
-
-		project.task('integrationTest', type: org.gradle.api.tasks.testing.Test) {
-			description = 'Runs the integration tests.'
-			group = 'verification'
-			testClassesDirs = project.sourceSets.integrationTest.output.classesDirs
-			classpath = project.sourceSets.integrationTest.runtimeClasspath
-			mustRunAfter project.tasks.'test'
-		}
-
-		project.check.dependsOn project.tasks.'integrationTest'
-
-		project.task('functionalTest', type: org.gradle.api.tasks.testing.Test) {
-			description = 'Runs the functional tests.'
-			group = 'verification'
-			testClassesDirs = project.sourceSets.functionalTest.output.classesDirs
-			classpath = project.sourceSets.functionalTest.runtimeClasspath
-			mustRunAfter project.tasks.'test'
-		}
-
-		project.check.dependsOn project.tasks.'functionalTest'
+//		project.sourceSets {
+//			integrationTest {
+//				java.srcDir project.file('src/integrationTest/java')
+//				resources.srcDir project.file('src/integrationTest/resources')
+//				compileClasspath += project.sourceSets.main.output + project.configurations.testRuntime
+//				runtimeClasspath += output + compileClasspath
+//			}
+//			functionalTest {
+//				java.srcDir project.file('src/functionalTest/java')
+//				resources.srcDir project.file('src/functionalTest/resources')
+//				compileClasspath += project.sourceSets.main.output + project.configurations.testRuntime
+//				runtimeClasspath += output + compileClasspath
+//			}
+//		}
+//
+//		project.task('integrationTest', type: org.gradle.api.tasks.testing.Test) {
+//			description = 'Runs the integration tests.'
+//			group = 'verification'
+//			testClassesDirs = project.sourceSets.integrationTest.output.classesDirs
+//			classpath = project.sourceSets.integrationTest.runtimeClasspath
+//			mustRunAfter project.tasks.'test'
+//		}
+//
+//		project.check.dependsOn project.tasks.'integrationTest'
+//
+//		project.task('functionalTest', type: org.gradle.api.tasks.testing.Test) {
+//			description = 'Runs the functional tests.'
+//			group = 'verification'
+//			testClassesDirs = project.sourceSets.functionalTest.output.classesDirs
+//			classpath = project.sourceSets.functionalTest.runtimeClasspath
+//			mustRunAfter project.tasks.'test'
+//		}
+//
+//		project.check.dependsOn project.tasks.'functionalTest'
 
 		project.afterEvaluate {
 			PublishingExtension publishingExtension = project.getExtensions().getByName(PublishingExtension.NAME);
