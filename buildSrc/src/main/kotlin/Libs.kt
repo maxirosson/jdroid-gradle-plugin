179cbf199f43a0581e926a6eff3f1d841d450d9a
Maxi Rosson <maxirosson@gmail.com>
2021-10-16 22:55:49 -0300

    Upgrade to Releases Hub v3.1.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -4,7 +4,7 @@ object Libs {
     const val JDROID_JAVA_CORE = "com.jdroidtools:jdroid-java-core:3.1.0"
 
     // https://github.com/dipien/releases-hub-gradle-plugin
-    const val RELEASES_HUB_GRADLE_PLUGIN = "com.dipien:releases-hub-gradle-plugin:3.0.0"
+    const val RELEASES_HUB_GRADLE_PLUGIN = "com.dipien:releases-hub-gradle-plugin:3.1.0"
 
     // https://plugins.gradle.org/plugin/com.gradle.plugin-publish
     const val GRADLE_PLUGIN_PUBLISH_PLUGIN = "com.gradle.publish:plugin-publish-plugin:0.13.0"
